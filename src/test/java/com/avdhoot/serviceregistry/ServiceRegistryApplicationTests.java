40b1bccb8b4e58bfb4b75ce765c531765ab793dc
AvdhootJadhav <avdhootjadhav002@gmail.com>
2021-07-02 19:41:44 +0530

    First Commit

---
+++ src/test/java/com/avdhoot/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avdhoot.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
